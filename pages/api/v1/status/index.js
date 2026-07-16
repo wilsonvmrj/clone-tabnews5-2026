7103709be9153d5997905e758fe0455dd4e8e97d
Wilson Magalhães <wilson.magalhaes@gmail.com>
2026-07-16 00:31:51 -0300

    test

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,8 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("select 1+1");
-  console.log(result);
-  response.status(200).json({ msg: "reponse do status paginação" });
+  const updatedAt = new Date().toISOString();
+  response.status(200).json({ updated_at: updatedAt });
 }
 
 export default status;
