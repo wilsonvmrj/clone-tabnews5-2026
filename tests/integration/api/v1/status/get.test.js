6104179d8a6570bfc2a4afc28088bc06496a7189
Wilson Magalhães <wilson.magalhaes@gmail.com>
2026-07-23 01:50:58 -0300

    test ci

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -14,7 +14,7 @@ test("GET to /api/v1/status should return 200", async () => {
   new Date(responseBody.updated_at).toISOString();
 
   expect(responseBody.dependencies.database.version).toBeDefined();
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.version).toEqual("20.0");
   expect(responseBody.dependencies.database.max_connections).toBeDefined();
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
